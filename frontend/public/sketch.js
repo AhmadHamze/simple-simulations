366703a731483dc98e00c893097024e3af2ab300
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-03-19 20:46:59 +0200

    Make a function that initializes a 2d array filled with random numbers

--- frontend/public/sketch.js
+++ frontend/public/sketch.js
@@ -1,12 +1,15 @@
+function initializeGrid(cols, rows) {
+  return new Array(cols).fill(0).map(() => new Array(rows).fill(0));
+}
+
 function setup() {
-    const canvas = createCanvas(400, 400);
-    canvas.parent("container");
-    background(0);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(255);
-      ellipse(mouseX, mouseY, 10, 10);
+  const canvas = createCanvas(400, 400);
+  canvas.parent("container");
+  const grid = initializeGrid(10, 10);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = floor(random(2));
     }
   }
+  console.table(grid);
+}
