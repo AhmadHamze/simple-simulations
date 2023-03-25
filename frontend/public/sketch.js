865807b644f27ab7607c40f0e002b2d84a6b3ed8
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-03-25 16:34:24 +0200

    Create a rectangular grid

--- frontend/public/sketch.js
+++ frontend/public/sketch.js
@@ -5,11 +5,33 @@ function initializeGrid(cols, rows) {
 function setup() {
   const canvas = createCanvas(400, 400);
   canvas.parent("container");
-  const grid = initializeGrid(10, 10);
+  gridHSize = 40;
+  gridVSize = 40;
+  grid = initializeGrid(gridVSize, gridHSize);
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      grid[i][j] = floor(random(2));
+      grid[i][j] = floor(random(2)); // 0 or 1
     }
   }
-  console.table(grid);
+}
+
+function drawRectangularCell(i, j, size) {
+  const x = i * size;
+  const y = j * size;
+  // If the grid is 1, draw a white rectangle
+  if (grid[i][j] === 1) {
+    fill(255);
+    stroke(0);
+    rect(x, y, size, size);
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      drawRectangularCell(i, j, height / gridHSize);
+    }
+  }
 }
