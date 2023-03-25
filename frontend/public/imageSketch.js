e51f8e965b8ef21e11ee1f2997ecba427f5d7595
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-03-25 15:50:41 +0200

    Try to implement parity rule one more time using the image approach, turns out way to complicated

--- frontend/public/imageSketch.js
+++ frontend/public/imageSketch.js
@@ -8,14 +8,14 @@ function setup() {
   pixelDensity(1);
 }
 
-function draw() {
-  background(51);
-
+function initializeOneBlackPixel() {
   loadPixels();
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      if (y % 2 === 0 && x % 2 === 0) {
+      if (y === height / 2 && x === width / 2) {
         const index = (x + y * width) * 4;
+        // "pixels" is a global variable it allows us to access the pixels of the canvas
+        // you need to change 4 values, the red, green, blue and alpha values
         pixels[index] = 0;
         pixels[index + 1] = 0;
         pixels[index + 2] = 0;
@@ -30,4 +30,12 @@ function draw() {
     }
   }
   updatePixels();
+}
+
+function draw() {
+  background(51);
+  // first, initialize the grid with one black pixel in the middle
+  initializeOneBlackPixel();
+  // It turns out this is hard to do, the grid way is much easier.
+  // the difficulty comes from the way the pixels are stored in the canvas
 }
