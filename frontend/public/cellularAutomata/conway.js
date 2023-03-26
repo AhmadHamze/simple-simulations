1e792ce8845ad523c2109e392ae79d946c03d95e
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-03-26 21:19:10 +0300

    Create a utils file and move some code to it

--- frontend/public/cellularAutomata/conway.js
+++ frontend/public/cellularAutomata/conway.js
@@ -1,7 +1,3 @@
-function initializeGrid(cols, rows) {
-  return new Array(cols).fill(0).map(() => new Array(rows).fill(0));
-}
-
 function setup() {
   const canvas = createCanvas(400, 400);
   canvas.parent("container");
@@ -15,23 +11,12 @@ function setup() {
   }
 }
 
-function drawRectangularCell(i, j, size) {
-  const x = i * size;
-  const y = j * size;
-  // If the grid is 1, draw a white rectangle
-  if (grid[i][j] === 1) {
-    fill(255);
-    stroke(0);
-    rect(x, y, size, size);
-  }
-}
-
 function draw() {
   background(51);
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      drawRectangularCell(i, j, height / gridHSize);
+      drawRectangularCell(grid, i, j, height / gridHSize);
     }
   }
 }
---
+++ frontend/public/cellularAutomata/utils.js
@@ -0,0 +1,14 @@
+function initializeGrid(cols, rows) {
+  return new Array(cols).fill(0).map(() => new Array(rows).fill(0));
+}
+
+function drawRectangularCell(grid, i, j, size) {
+  const x = i * size;
+  const y = j * size;
+  // If the grid is 1, draw a white rectangle
+  if (grid[i][j] === 1) {
+    fill(255);
+    stroke(0);
+    rect(x, y, size, size);
+  }
+}
