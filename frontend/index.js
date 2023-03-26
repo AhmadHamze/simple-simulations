8fbc8ffc680a8db56b2e6fe17a6afc67ff0a5b43
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-03-26 15:32:02 +0300

    make changes to the project structure. create a static folder containing a index.js file which is the entry point of the spa

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(5000, () => {
-    console.log('Listening on port 5000');
-})
+  console.log("Listening on port 5000");
+});
