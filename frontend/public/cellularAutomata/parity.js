d160ca44a9360dc5414b47d347fa6750178d2a76
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-04-06 21:58:06 +0300

    Add the parity sketch, working fine

---
+++ frontend/public/cellularAutomata/parity.js
@@ -0,0 +1,46 @@
+function setup() {
+  frameRate(1);
+  const canvas = createCanvas(400, 400);
+  canvas.parent("container");
+  gridHSize = 40;
+  gridVSize = 40;
+  grid = initializeGrid(gridVSize, gridHSize);
+  // Create a grid with a single 0 in the middle
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i === gridHSize / 2 && j === gridVSize / 2) {
+        grid[i][j] = 0;
+        continue;
+      }
+      grid[i][j] = 1;
+    }
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      drawRectangularCell(grid, i, j, height / gridHSize);
+    }
+  }
+
+  for (let i = 0; i < 32; i++) {
+    const gridCopy = grid.map((arr) => [...arr]);
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        iM = (i - 1) % grid.length;
+        iP = (i + 1) % grid.length;
+        jM = (j - 1) % grid[i].length;
+        jP = (j + 1) % grid[i].length;
+        grid[i][j] =
+          (gridCopy.at(iM).at(j) +
+            gridCopy.at(iP).at(j) +
+            gridCopy.at(i).at(jM) +
+            gridCopy.at(i).at(jP)) %
+          2;
+      }
+    }
+  }
+}
