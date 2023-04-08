4be33eb98f9bf317b4fcb34370ac63813d59f8e8
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-04-08 14:37:28 +0300

    Fix the rule and remove the while loop, turns out there is no need for it

--- frontend/public/cellularAutomata/parity.js
+++ frontend/public/cellularAutomata/parity.js
@@ -2,7 +2,8 @@ function setup() {
   frameRate(1);
   const canvas = createCanvas(400, 400);
   canvas.parent("container");
-  maxIter = 64;
+  iter = 0;
+  iteration = createP("Iteration: ");
   gridHSize = 40;
   gridVSize = 40;
   grid = initializeGrid(gridVSize, gridHSize);
@@ -26,23 +27,22 @@ function draw() {
       drawRectangularCell(grid, i, j, height / gridHSize);
     }
   }
-  let k = 0;
-  while (k <= maxIter) {
-    const gridCopy = grid.map((arr) => [...arr]);
-    for (let i = 0; i < gridVSize; i++) {
-      for (let j = 0; j < gridHSize; j++) {
-        iM = (i - 1) % gridVSize;
-        iP = (i + 1) % gridVSize;
-        jM = (j - 1) % gridHSize;
-        jP = (j + 1) % gridHSize;
-        grid[i][j] =
-          (gridCopy.at(iM).at(j) +
-            gridCopy.at(iP).at(j) +
-            gridCopy.at(i).at(jM) +
-            gridCopy.at(i).at(jP)) %
-          2;
-      }
+  iter++;
+  iteration.html("Iteration: " + iter);
+  const gridCopy = grid.map((arr) => [...arr]);
+  for (let i = 0; i < gridVSize; i++) {
+    for (let j = 0; j < gridHSize; j++) {
+      iM = (i - 1) % gridVSize;
+      iP = (i + 1) % gridVSize;
+      jM = (j - 1) % gridHSize;
+      jP = (j + 1) % gridHSize;
+      grid[i][j] =
+        (1 +
+          gridCopy.at(iM).at(j) +
+          gridCopy.at(iP).at(j) +
+          gridCopy.at(i).at(jM) +
+          gridCopy.at(i).at(jP)) %
+        2;
     }
-    k++;
   }
 }
