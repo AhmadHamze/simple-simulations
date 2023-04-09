a6a0bcd3b7d68a7376eed57c522f3443dc1155f2
Ahmad Hamze <ahmadhamze@yahoo.com>
2023-04-09 14:39:18 +0300

    Create a Cell class to represent a cell

--- frontend/public/cellularAutomata/utils.js
+++ frontend/public/cellularAutomata/utils.js
@@ -12,3 +12,28 @@ function drawRectangularCell(grid, i, j, size) {
     rect(x, y, size, size);
   }
 }
+
+/**
+ * The Cell class represents a cell in the grid.
+ * White cells are dead, black cells are alive.
+ * @class
+ * @param {number} x The x coordinate of the cell
+ * @param {number} y The y coordinate of the cell
+ * @param {number} size The size of the cell
+ * @param {number} alive The state of the cell, 0 if dead, 1 if alive
+ */
+class Cell {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.alive = 0;
+  }
+  drawCell() {
+    if (!this.alive) {
+      fill(255);
+      stroke(200);
+      rect(this.x, this.y, this.size, this.size);
+    }
+  }
+}
